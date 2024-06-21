c584d3fdbb274a17c43f6cb04b88838451f2dde0
jola-blvhk <awujoolabello@gmail.com>
2024-06-21 14:13:09 +0100

    molang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Spline from "@splinetool/react-spline/next";
 
-
-
 export default function Home() {
   return (
-    <main className={`relative ${poppins.variable}`}>
+    <main className={`relative `}>
       <div className="text-3xl absolute">Hi, my name is Molang</div>
       <Spline scene="https://prod.spline.design/wsxc8rGYlBFWzzhG/scene.splinecode" />
     </main>
